b5004099720e28e10f02f750e254e456438b37e9
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-05-06 11:34:06 -0300

    first try 🚧

---
+++ BlinkingRandomLabel/Classes/BlinkingRandomLabel.swift
@@ -0,0 +1,22 @@
+//
+//  BlinkingRandomLabel.swift
+//  BlinkingRandomLabel
+//
+//  Created by Marcelo Sampaio on 06/05/22.
+//
+
+import Foundation
+
+public class BlinkingRandomLabel : UILabel {
+    public func startBlinking() {
+        let options : UIViewAnimationOptions = .repeat //.Repeat | .Autoreverse
+        UIView.animate(withDuration: 0.25, delay:0.0, options:options, animations: {
+            self.alpha = 0
+            }, completion: nil)
+    }
+ 
+    public func stopBlinking() {
+        alpha = 1
+        layer.removeAllAnimations()
+    }
+}
--- Example/BlinkingRandomLabel/ViewController.swift
+++ Example/BlinkingRandomLabel/ViewController.swift
@@ -7,18 +7,39 @@
 //
 
 import UIKit
-
+import BlinkingRandomLabel
+ 
 class ViewController: UIViewController {
-
+ 
+    var isBlinking = false
+    let blinkingLabel = BlinkingRandomLabel(frame: CGRect(x: 10, y: 20, width: 200, height: 30))
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+ 
+        // Setup the BlinkingRandomLabel
+        blinkingLabel.text = "Blinking Label!"
+        blinkingLabel.font = UIFont.systemFont(ofSize: 20) // UIFont.systemFontOfSize(20)
+        view.addSubview(blinkingLabel)
+        blinkingLabel.startBlinking()
+        isBlinking = true
+ 
+        // Create a UIButton to toggle the blinking
+        let toggleButton = UIButton(frame: CGRect(x: 10, y: 60, width: 125, height: 30))
+        toggleButton.setTitle("Toggle Blinking", for: .normal)
+        toggleButton.setTitleColor(.red, for: .normal)
+        toggleButton.addTarget(self, action: #selector(toggleBlinking), for: .touchUpInside)
+        view.addSubview(toggleButton)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ 
+    @objc func toggleBlinking() {
+        if (isBlinking) {
+            blinkingLabel.stopBlinking()
+        } else {
+            blinkingLabel.startBlinking()
+        }
+        isBlinking = !isBlinking
     }
-
+ 
 }
 
